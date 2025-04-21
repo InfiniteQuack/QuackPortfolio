127abb5d46b1bf73ae279fc91a9822296ea6a531
Juno Skutelnik <skutelnik5109@saskpolytech.ca>
2025-04-20 21:08:38 -0600

    added a fucking font at long last

---
+++ tcom/src/components/Main.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="Roboto">
+
+</div>
+</template>
+
+<style scoped>
+  @import url(
+  "https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+  );
+  
+  .Roboto {
+    font-family: 'Roboto Mono', sans-serif;
+  }
+</style>
